f81530f6dbcd514705c032b1b7af769fd664e623
atheimuz <atheimuz@naver.com>
2024-08-23 20:16:17 +0900

    emotion theme 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+import { Theme } from "@emotion/react";
+
+export const color = {
+    active: "#e50914",
+
+    gray1: "#e4e4e4",
+    gray2: "#bbbbbb",
+    gray3: "#a3a3a3",
+    gray4: "#404040",
+    gray5: "#1e1e1e",
+};
+
+export const indent = {
+    horizontal: "32px",
+};
+
+const theme: Theme = {
+    color,
+    indent,
+};
+
+export default theme;
